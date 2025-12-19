e99b8409dc3d982e1bf4b74112e4f69f2f2220a6
Paulo Souza <42076064+paulosouza4u@users.noreply.github.com>
2025-12-19 00:00:37 -0300

    up

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
     experimental: {
         globalNotFound: true,
     },
+    env: {
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || "",
+        FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL || "",
+        FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY || "",
+    },
 };
 
 export default nextConfig;
